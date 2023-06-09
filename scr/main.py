06a99a2b0c65ff2d5fb5362e2e0319e49259ca4b
waybro <a.ultanov@bk.ru>
2023-06-09 13:44:13 +0700

    get histories price

--- scr/main.py
+++ scr/main.py
@@ -4,18 +4,55 @@ import os
 from binance import AsyncClient, BinanceSocketManager
 from dotenv import load_dotenv
 
+# from .constants import BARS, TF
+
 load_dotenv()
+
+in_symbols = (
+    # 1 символ,  второй,   ТФ,   количество баров
+    ('BTCUSDT', 'ETHUSDT', '1h', 200),
+    ('SOLUSDT', 'TRXUSDT', '1h', 200),
+)
 
 API_KEY = os.getenv('API_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-async def get_exchange_info_async(cl: AsyncClient = None):
-    result = await cl.get_exchange_info()
-    print(result)
+
+async def async_get_historical_klines(symbols: tuple, cl: AsyncClient):
+    """Получаем данные о монетах из кортежа."""
+    result_1 = await cl.get_historical_klines(symbols[0], interval=symbols[2], limit=symbols[3])
+    result_2 = await cl.get_historical_klines(symbols[1], interval=symbols[2], limit=symbols[3])
+    print('Работу закончил')
+    return result_1, result_2
+
+async def get_price_symbol(symbols: tuple, bm: BinanceSocketManager, tf: str):
+    print(symbols)
+    ts_p = bm.kline_futures_socket(symbols, interval=tf)
+    async with ts_p as tsbm:
+        while True:
+            res = await tsbm.recv()
+            print(res)
+
 
 async def main(cl: AsyncClient):
-    task = loop.create_task(get_exchange_info_async(cl))
-    await asyncio.wait([task])
+    """ Основаная функция проекта"""
+    bm = BinanceSocketManager(cl)                             # Создали экземпляр вебсокета
+
+    tasks_kline = [                                           # Создали список тасков
+        (loop.create_task(
+            async_get_historical_klines(symbols, cl),         # Перебираем кортеж in_symbols  
+        )) for symbols in in_symbols if in_symbols
+    ]
+    kline_symbol_bars = [await task for task in tasks_kline]  # Запускаем таски
+
+    price_symbol = [
+        get_price_symbol(
+            symbol, bm, symbols[2]
+        ) for symbols in in_symbols 
+            for symbol in symbols[:2]
+                if in_symbols
+    ]
+
 
 
 if __name__ == '__main__':
